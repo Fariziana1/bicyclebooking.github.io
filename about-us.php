f2e87e1a27f38e6700c28722a22d1c47c3cb7363
Fariziana1 <152669138+Fariziana1@users.noreply.github.com>
2024-01-03 16:14:07 +0800

    Add files via upload
    ok

---
+++ about-us.php
@@ -0,0 +1,323 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<title>Bicycle Rental | About Us</title>
+<!--Bootstrap -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+<!--Custome Style -->
+<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+<!--OWL Carousel slider-->
+<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
+<!--slick-slider -->
+<link href="assets/css/slick.css" rel="stylesheet">
+<!--bootstrap-slider -->
+<link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+<!--FontAwesome Font Style -->
+<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+
+
+<!-- Fav and touch icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+</head>
+<body>
+
+
+<!--Header-->
+
+<header>
+  <div class="default-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3 col-md-2">
+          <div class="logo"> <a href="index.php"><img src="assets/images/logo-header.png" alt="image"/></a> </div>
+        </div>
+        <div class="col-sm-9 col-md-10">
+          <div class="header_info">
+            <div class="header_widgets">
+              <div class="circle_icon"> <i class="fa fa-envelope" aria-hidden="true"></i> </div>
+              <p class="uppercase_text">EMAIL SUPPORT: </p>
+              uniszabikerental@gmail.com </div>
+            <div class="header_widgets">
+              <div class="circle_icon"> <i class="fa fa-phone" aria-hidden="true"></i> </div>
+              <p class="uppercase_text">CALL Staff: </p>
+              60-19874552 </div>
+            <div class="social-follow">
+             
+            </div>
+   Welcome To Unisza Bike Rental!          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Navigation -->
+  
+  <!-- Navigation end -->
+
+</header>
+                      <section class="page-header aboutus_page">
+  <div class="container">
+    <div class="page-header_wrap">
+      <div class="page-heading">
+        <h1>Terms and Conditions</h1>
+      </div>
+      <ul class="coustom-breadcrumb">
+        <li><a href="index.php">Home</a></li>
+        <li>Terms and Conditions</li>
+      </ul>
+    </div>
+  </div>
+  <!-- Dark Overlay-->
+  <div class="dark-overlay"></div>
+</section>
+<section class="about_us section-padding">
+  <div class="container">
+    <div class="section-header text-center">
+
+
+      <h2>Terms and Conditions</h2>
+      <p>																																																																						<p align="justify"><font size="2">
+Welcome to our Bicycle Booking System! These terms and conditions outline the rules and regulations for the use of our bicycle rental services. By accessing or using our platform, you agree to comply with these terms. Please read them carefully before making a booking.
+<br><br></font><span style="color: rgb(0, 140, 255); font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-weight: 700; text-align: start; white-space: pre; background-color: rgb(248, 248, 248);">(1) Maintenance, Tuning and Responsibility</span><font size="2"><strong><br><br></strong>
+
+By accepting the bike(s)/ equipment the student accepts that they are in good technical, safe and clean condition and must be returned in the same condition. Bikes and equipment hired remain the property of Bike Rental. Students are responsible for all cycles and equipment hired and it student responsibility to keep these items safe from damage, loss or theft, use it in a proper manner and not subject it to any misuse or unfair wear and tear. Students are strongly advised not to leave the bike unattended in any public place, if student do leave the bike unattended it must be locked to a permanent fixture.
+</font><font size="2"><br><br></font><span style="color: rgb(0, 140, 255); font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-weight: 700; text-align: start; white-space: pre; background-color: rgb(248, 248, 248);">(2) Responsibility for Damage or Loss</span><font size="2"><strong><br><br></strong>
+
+Student agrees will return the equipment in the same good condition as when received, ordinary wear and tear accepted, and to repair and replace lost or stolen, damaged or broken bicycles or parts or to reimburse Bike Rentals for said equipment. Therefore, regardless of the party at fault, student understands and agrees to be responsible for the theft or damage to said equipment. A refundable deposit is required upon borrowing a bicycle to ensure its return in good condition. Upon satisfactory return of the bicycle, the deposited amount will be refunded. Failure to return the bicycle in good condition may result in the forfeiture of the deposit.									
+		 </p>
+		 
+    </div>
+     </div>
+</section>
+<!-- /About-us--> 
+
+
+
+
+
+<!--Footer -->
+
+<footer>
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6">
+          <h6>More Info</h6>
+          <ul>
+            <li><a href="about-us.php?type=terms">Terms and Conditions</a></li>
+            <li><a target="_blank" href="admin/">Admin Login</a></li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </div>
+ 
+</footer>
+<!-- /Footer--> 
+
+<!--Back to top-->
+<div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+<!--/Back to top--> 
+
+<!--Login-Form -->
+
+<div class="modal fade" id="loginform">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h3 class="modal-title">Login</h3>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="login_wrap">
+            <div class="col-md-12 col-sm-6">
+              <form method="post">
+                <div class="form-group">
+                  <input type="email" class="form-control" name="email" placeholder="Email Address*">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" name="password" placeholder="Password*">
+                </div>
+                <div class="form-group checkbox">
+                  <input type="checkbox" id="remember">
+               
+                </div>
+                <div class="form-group">
+                  <input type="submit" name="login" value="Login" class="btn btn-block">
+                </div>
+              </form>
+            </div>
+           
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer text-center">
+        <p>Don't have an account? <a href="#signupform" data-toggle="modal" data-dismiss="modal">Sign-Up Here</a></p>
+        <p><a href="#forgotpassword" data-toggle="modal" data-dismiss="modal">Forgot Password?</a></p>
+      </div>
+    </div>
+  </div>
+</div><!--/Login-Form --> 
+
+<!--Register-Form -->
+
+
+<script>
+function checkAvailability() {
+$("#loaderIcon").show();
+jQuery.ajax({
+url: "check_availability.php",
+data:'emailid='+$("#emailid").val(),
+type: "POST",
+success:function(data){
+$("#user-availability-status").html(data);
+$("#loaderIcon").hide();
+},
+error:function (){}
+});
+}
+</script>
+<script type="text/javascript">
+function valid()
+{
+if(document.signup.password.value != document.signup.confirmpassword.value)
+{
+alert("Password and Confirm Password Field do not match  !!");
+document.signup.confirmpassword.focus();
+return false;
+}
+return true;
+}
+</script>
+<div class="modal fade" id="signupform">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h3 class="modal-title">Sign Up</h3>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="signup_wrap">
+            <div class="col-md-12 col-sm-6">
+              <form  method="post" name="signup" onSubmit="return valid();">
+                <div class="form-group">
+                  <input type="text" class="form-control" name="fullname" placeholder="Full Name*" required>
+                </div>
+                      <div class="form-group">
+                  <input type="text" class="form-control" name="mobileno" placeholder="Mobile Number*" required>
+                </div>
+                <div class="form-group">
+                  <input type="email" class="form-control" name="emailid" id="emailid" onBlur="checkAvailability()" placeholder="Email Address*" required>
+                   <span id="user-availability-status" style="font-size:12px;"></span> 
+                </div>
+                <div class="form-group">
+                  <input type="password" name="password" class="form-control" placeholder="Password*" required>
+                </div>
+                <div class="form-group">
+                  <input type="password" name="confirmpassword" class="form-control" placeholder="Confirm Password*" required>
+                </div>
+                <div class="form-group checkbox">
+                  <input type="checkbox" id="terms_agree" required>
+                  <label for="terms_agree">I Agree with <a href="about-us.php?type=terms" target="_blank">Terms and Conditions</a></label>
+                </div>
+                <div class="form-group">
+                  <input type="submit" value="Sign Up" name="signup" id="submit" class="btn btn-block">
+                </div>
+              </form>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer text-center">
+        <p>Already got an account? <a href="#loginform" data-toggle="modal" data-dismiss="modal">Login Here</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+<!--/Register-Form --> 
+
+<!--Forgot-password-Form -->
+  <script type="text/javascript">
+function valid()
+{
+if(document.chngpwd.newpassword.value!= document.chngpwd.confirmpassword.value)
+{
+alert("New Password and Confirm Password Field do not match  !!");
+document.chngpwd.confirmpassword.focus();
+return false;
+}
+return true;
+}
+</script>
+<div class="modal fade" id="forgotpassword">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h3 class="modal-title">Forgot Password</h3>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="forgotpassword_wrap">
+            <div class="col-md-12">
+              <form name="chngpwd" method="post" onSubmit="return valid();">
+                <div class="form-group">
+                  <input type="email" name="email" class="form-control" placeholder="Current Email Address*" required>
+                </div>
+  <div class="form-group">
+                  <input type="text" name="mobile" class="form-control" placeholder="Current Mobile Number*" required>
+                </div>
+  <div class="form-group">
+                  <input type="password" name="newpassword" class="form-control" placeholder="New Password*" required>
+                </div>
+  <div class="form-group">
+                  <input type="password" name="confirmpassword" class="form-control" placeholder="Confirm Password*" required>
+                </div>
+                <div class="form-group">
+                  <input type="submit" value="Reset Password" name="update" class="btn btn-block">
+                </div>
+              </form>
+              <div class="text-center">
+                <p class="black_text">We do not store your password for security reasons. We will reset your password and send a new one to you.</p>
+                <p><a href="#loginform" data-toggle="modal" data-dismiss="modal"><i class="fa fa-angle-double-left" aria-hidden="true"></i> Back to Login</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div><!--/Forgot-password-Form --> 
+
+<!-- Scripts --> 
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/interface.js"></script> 
+
+<!--bootstrap-slider-JS--> 
+<script src="assets/js/bootstrap-slider.min.js"></script> 
+<!--Slider-JS--> 
+<script src="assets/js/slick.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+
+</html>
